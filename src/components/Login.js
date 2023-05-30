dcf69233dc886208a5ff2d6cbfd925ba2c60a118
9th Symphony <bordadora67@gmail.com>
2023-05-30 21:54:13 +0800

    GUI Signup and Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,53 +29,102 @@ function Login() {
     }
   }
 
+  const appContainerStyle = {
+    textAlign: 'center',
+    display: 'flex',
+    minHeight: '100vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundImage: 'linear-gradient(50deg, #1b6d22, #24993d 48%, #40a82b)',
+  };
+  
+  const authFormContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '5rem',
+    border: '1px solid white',
+    borderRadius: '10px',
+    margin: '0.5rem',
+  };
+  
+  const labelStyle = {
+    textAlign: 'left',
+    padding: '0.50rem 0',
+  };
+  
+  const inputStyle = {
+    margin: '0.50rem 0',
+    padding: '1rem',
+    border: 'none',
+    borderRadius: '10px',
+  };
+  
+  const buttonStyle = {
+    border: 'none',
+    backgroundColor: 'aliceblue',
+    padding: '15px',
+    borderRadius: '20px',
+    cursor: 'pointer',
+    color: '#103618',
+  };
+  
+  const linkButtonStyle = {
+    background: 'none',
+    color: 'whitesmoke',
+    textDecoration: 'underline',
+  };
+  
   return (
-    <div className="App">
-      <div className="auth-form-container">
-        <Card className="auth-card" style={{ backgroundColor: "green", color: "white" }}>
+    <div className="App" style={appContainerStyle}>
+      <div className="auth-form-container" style={authFormContainerStyle}>
+        <Card className="auth-card">
           <Card.Body>
             <Form>
               <Row className="mb-3">
-                <Form.Label column sm={3}>Email address</Form.Label>
+                <Form.Label column sm={3} style={labelStyle}>Email address</Form.Label>
                 <Col sm={9}>
                   <Form.Control
                     type="email"
                     placeholder="Enter email"
                     onChange={(e) => {
                       setEmail(e.target.value);
-                      setErrorMessage(""); // Clear error message when input changes
+                      setErrorMessage(''); // Clear error message when input changes
                     }}
+                    style={inputStyle}
                   />
                 </Col>
               </Row>
-
+  
               <Row className="mb-3">
-                <Form.Label column sm={3}>Password</Form.Label>
+                <Form.Label column sm={3} style={labelStyle}>Password</Form.Label>
                 <Col sm={9}>
                   <Form.Control
                     type="password"
                     placeholder="Password"
                     onChange={(e) => {
                       setPassword(e.target.value);
-                      setErrorMessage(""); // Clear error message when input changes
+                      setErrorMessage(''); // Clear error message when input changes
                     }}
+                    style={inputStyle}
                   />
                 </Col>
               </Row>
-
+  
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+                <Form.Check type="checkbox" label="Remember Me" />
               </Form.Group>
-
-              <Button variant="secondary" type="submit" onClick={submit}>
+  
+              <Button variant="secondary" type="Log in" onClick={submit} style={buttonStyle}>
                 Submit
               </Button>
             </Form>
+            <Link to="/signup">Signup Page</Link>
           </Card.Body>
         </Card>
       </div>
     </div>
-  );
+  );  
 }
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -38,23 +38,107 @@ function Login() {
 
             }
         }
-    return (
-        <div className ="login">
+
+        const appContainerStyle = {
+          textAlign: 'center',
+          display: 'flex',
+          minHeight: '100vh',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundImage: 'linear-gradient(50deg, #1b6d22, #24993d 48%, #40a82b)',
+        };
+        
+        const authFormContainerStyle = {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '2rem',
+          border: '1px solid white',
+          borderRadius: '10px',
+          margin: '0.5rem',
+        };
+        
+        const labelStyle = {
+          textAlign: 'left',
+          padding: '0.5rem 0',
+        };
+        
+        const inputStyle = {
+          margin: '0.5rem 0',
+          padding: '0.5rem',
+          border: 'none',
+          borderRadius: '10px',
+        };
+        
+        const buttonStyle = {
+          border: 'none',
+          backgroundColor: 'aliceblue',
+          padding: '1rem',
+          borderRadius: '20px',
+          cursor: 'pointer',
+          color: '#103618',
+          marginTop: '0.5rem',
+        };
+        
+        const linkButtonStyle = {
+          background: 'none',
+          color: 'whitesmoke',
+          textDecoration: 'underline',
+          marginTop: '1rem',
+        };
+        
+        return (
+          <div className="login" style={appContainerStyle}>
+            
+        
+            
+            <form action="POST" style={authFormContainerStyle}>
             <h1>Signup</h1>
-
-            <form action="POST">
-                <input type="email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email" name="" id="" /> 
-                <input type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password" name="" id="" />  
-
-                 <input type="submit" onClick={submit} />
-            </form>
-
-            <br />
+            <label style={labelStyle}>Full Name</label>
+              <input
+                type="fullname"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                placeholder="Fullname"
+                name=""
+                id=""
+                style={inputStyle}
+              />
+              <label style={labelStyle}>Email</label>
+              <input
+                type="email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                placeholder="Email"
+                name=""
+                id=""
+                style={inputStyle}
+              />
+        
+              <label style={labelStyle}>Password</label>
+              <input
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                placeholder="Password"
+                name=""
+                id=""
+                style={inputStyle}
+              />
+        
+              <input type="submit" onClick={submit} style={buttonStyle} />
+              <br />
             <p>OR</p>
             <br />
-
-            <Link to="/">Login Page</Link>
-        </div>
-    )
+        
+              <Link to="/" style={linkButtonStyle}>Login Page</Link>
+            </form>
+        
+            
+    </div>
+ );        
 }
 export default Login
